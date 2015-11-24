20a9b9b860bf9b9a57b0c919b539fd7715ea3a88
sermodi <sergiomodino@hotmail.com>
2015-11-24 19:12:28 +0100

    Activity de lista de Rutas

--- app/src/main/java/com/asimov/sportroutes/Fragment_RealizarRuta.java
+++ app/src/main/java/com/asimov/sportroutes/Fragment_RealizarRuta.java
@@ -4,11 +4,13 @@ package com.asimov.sportroutes;
  * Fragmento que muestra el boton realizar ruta
  * Created by Sarekito on 16/11/15.
  */
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class Fragment_RealizarRuta extends Fragment {
 
@@ -19,7 +21,16 @@ public class Fragment_RealizarRuta extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View inflador = inflater.inflate(R.layout.fragment_fragment2, container, false);
+        Button boton = (Button) inflador.findViewById(R.id.buttonRealizar);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(),ListaDeRutasActivity.class);
+                startActivity(intent);
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment2, container, false);
+        return inflador;
     }
 }
---
+++ app/src/main/java/com/asimov/sportroutes/ListaDeRutasActivity.java
@@ -0,0 +1,13 @@
+package com.asimov.sportroutes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaDeRutasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_de_rutas);
+    }
+}
