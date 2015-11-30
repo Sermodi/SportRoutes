e6cbcee1d9d3c7865978b1d09fae8e73543871c1
sermodi <sergiomodino@hotmail.com>
2015-11-30 15:38:31 +0100

    Activity de lista de Rutas

--- app/src/main/java/com/asimov/sportroutes/ListaDeRutasActivity.java
+++ app/src/main/java/com/asimov/sportroutes/ListaDeRutasActivity.java
@@ -2,6 +2,8 @@ package com.asimov.sportroutes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class ListaDeRutasActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class ListaDeRutasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_de_rutas);
+        TableLayout tabla = (TableLayout) findViewById(R.id.tablaRutas);
+        TableRow fila = (TableRow) findViewById(R.id.Fila);
+
     }
 }
