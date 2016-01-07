c58300e0882b068a73a3c151fb8fbd80565c7b5d
Sarekito <sanlumberjack@gmail.com>
2016-01-08 00:00:51 +0100

    changes

--- app/src/main/java/com/asimov/sportroutes/Fragments/Fragment_Manual.java
+++ app/src/main/java/com/asimov/sportroutes/Fragments/Fragment_Manual.java
@@ -22,6 +22,6 @@ public class Fragment_Manual extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment4, container, false);
+        return inflater.inflate(R.layout.how_to_activity, container, false);
     }
 }
